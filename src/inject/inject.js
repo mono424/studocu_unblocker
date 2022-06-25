e1c7889b3025f4023efaaa0bc50fb5e52d609daf
Khadim <khadimfall0602@gmail.com>
2022-06-25 20:05:19 +0200

    fixed :)

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,15 +4,31 @@
 	style.innerText = "div { filter: none !important; }";
 	document.head.appendChild(style);
 
-	document.querySelectorAll("#viewer-wrapper > div").forEach(e => {
-		if (e.id == '' && e.getAttribute('role') == null) {
+	document.querySelectorAll("#document-wrapper > div").forEach(e => {
+		const computedStyleMap = e.computedStyleMap();
+		if (computedStyleMap.get('position').value.toLowerCase() == "sticky") {
 			e.remove();
 		}
 	})
 
-	document.querySelectorAll("#viewer-wrapper .pf > div").forEach(e => {
-		if (e.innerHTML.toLowerCase().indexOf("premium") > -1) {
+	document.querySelectorAll("#document-wrapper #premium-page-header").forEach(e => {
+		e.remove();
+	})
+
+	document.querySelectorAll("#document-wrapper .pf > div").forEach(e => {
+		if (e.innerHTML.toLowerCase().indexOf("premium") >= 0) {
 			e.remove();
+		}
+	})
+
+	document.querySelectorAll("button").forEach(e => {
+		if (e.innerHTML.toLowerCase().indexOf("herunterladen") >= 0) {
+			var cloned = e.cloneNode(true);
+			e.parentNode.replaceChild(cloned, e);
+			cloned.onclick = (e) => {
+				e.preventDefault();
+				alert("Not possible yet, feel free to create a pr on github ;)");
+			};
 		}
 	})
 
