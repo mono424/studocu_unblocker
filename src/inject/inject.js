8a30606b8e8049f4eec39715c7169f40b728bcce
Khadim <khadimfall0602@gmail.com>
2024-02-17 22:47:32 +0100

    Fixed

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,112 +1,156 @@
-(function() {
+(async function() {
 
-	var style = document.createElement('style');
-	style.innerText = "div { filter: none !important; }";
-	document.head.appendChild(style);
-
-	document.querySelectorAll("#document-wrapper > div").forEach(e => {
-		const computedStyleMap = e.computedStyleMap();
-		if (computedStyleMap.get('position').value.toLowerCase() == "sticky") {
-			e.remove();
+	let documentWrapper = null;
+	let maxTries = 1000;
+	while (documentWrapper == null) {
+		documentWrapper = document.getElementById("document-wrapper");
+		await new Promise(r => setTimeout(r, 100));
+		if (maxTries-- <= 0) {
+			console.error("StuDocu Unblocker: Could not find document wrapper!");
+			return;
 		}
-	})
+	}
 
-	document.querySelectorAll("#document-wrapper #premium-page-header").forEach(e => {
-		e.remove();
-	})
-
-	document.querySelectorAll("#document-wrapper .pf > div").forEach(e => {
-		if (e.innerHTML.toLowerCase().indexOf("premium") >= 0) {
-			e.remove();
+	const globalStyle = document.createElement('style');
+	globalStyle.innerText = `
+		.unblock-button {
+			margin: 0 0 0 10px;
+			padding: 7px 24px;
+			border: none;
+			background-color: #61c4ff;
+			color: white;
+			cursor: pointer;
+			font-weight: bold;
+			border-radius: 32px;
 		}
-	})
-
-	document.querySelectorAll("button").forEach(e => {
-		if (e.innerHTML.toLowerCase().indexOf("herunterladen") >= 0) {
-			var cloned = e.cloneNode(true);
-			e.parentNode.replaceChild(cloned, e);
-			cloned.onclick = (e) => {
-				e.preventDefault();
-				printPdf();
-			};
+		.unblock-button:hover {
+			background-color: #4da6ff;
 		}
-	})
+		.unblock-button:active {
+			background-color: #3b8ad9;
+		}
+	`;
+	document.head.appendChild(globalStyle);
 
-	function printPdf() {
-		const originalContents = document.body.innerHTML;
-		const extraStyles = `
-			<style>
-				@page {
-					size: auto;
-					margin: 0;
-				}
+	const header = document.querySelector("header > div");
 
-				@media print {
-					#main-wrapper .page-content-wrapper #viewer-wrapper {
-						display: flex !important;
-						background: white !important;
-					}
+	const bypassButton = document.createElement('button');
+	bypassButton.innerText = "🔓 Unlock Document";
+	bypassButton.className = "unblock-button";
+	bypassButton.onclick = unblock;
+	header.appendChild(bypassButton);
 
-					#main-wrapper .page-content-wrapper #document-wrapper {
-						overflow: none;
-						height: auto;
-						display: block !important;
-					}
+	console.log("StuDocu Unblocker: Injected!");
 
-					#main-wrapper .page-content-wrapper #document-wrapper > div {
-						height: auto !important;
-						margin-top: 0 !important;
-						margin-bottom: 0 !important;
-					}
-
-					#main-wrapper .page-content-wrapper #page-container-wrapper {
-						transform: scale(1) !important;
-						height: auto !important;
-						width: auto !important;
-					}
-
-					#main-wrapper .page-content-wrapper #page-container > div {
-						margin: 0 !important;
-					}
-
-					#main-wrapper {
-						width: 100vw !important;
-						height: auto !important;
-					}
-				}
-
-				@media screen {
-					#main-wrapper .page-content-wrapper * {
-						display: none !important;
-					}
-				}
-			</style>
-		`;
-
-		document.querySelectorAll("#main-wrapper > *").forEach(e => {
-			if (!e.classList.contains("page-content-wrapper")) {
-				e.remove();
-			}
-		});
-
-		document.querySelectorAll("#main-wrapper .page-content-wrapper > *").forEach(e => {
-			if (e.id != "viewer-wrapper") {
-				e.remove();
-			}
-		});
-
-		document.querySelectorAll("#main-wrapper .page-content-wrapper #viewer-wrapper > *").forEach(e => {
-			if (e.id != "document-wrapper") {
+	function unblock() {
+		var style = document.createElement('style');
+		style.innerText = "div { filter: none !important; }";
+		document.head.appendChild(style);
+	
+		document.querySelectorAll("#document-wrapper > div > div").forEach(e => {
+			const computedStyleMap = e.computedStyleMap();
+			if (computedStyleMap.get('position').value.toLowerCase() == "sticky") {
 				e.remove();
 			}
 		})
-
-		document.querySelector("footer").remove();
-
-		document.body.innerHTML += extraStyles;
-		
-		window.print();
-		document.body.innerHTML = originalContents;
+	
+		document.querySelectorAll("#document-wrapper #premium-page-header").forEach(e => {
+			e.remove();
+		})
+	
+		document.querySelectorAll("#document-wrapper .pf > div").forEach(e => {
+			if (e.innerHTML.toLowerCase().indexOf("premium") >= 0) {
+				e.remove();
+			}
+		})
+	
+		document.querySelectorAll("button").forEach(e => {
+			if (e.innerHTML.toLowerCase().indexOf("herunterladen") >= 0) {
+				var cloned = e.cloneNode(true);
+				e.parentNode.replaceChild(cloned, e);
+				cloned.onclick = (e) => {
+					e.preventDefault();
+					printPdf();
+				};
+			}
+		})
+	
+		function printPdf() {
+			const originalContents = document.body.innerHTML;
+			const extraStyles = `
+				<style>
+					@page {
+						size: auto;
+						margin: 0;
+					}
+	
+					@media print {
+						#main-wrapper .page-content-wrapper #viewer-wrapper {
+							display: flex !important;
+							background: white !important;
+						}
+	
+						#main-wrapper .page-content-wrapper #document-wrapper {
+							overflow: none;
+							height: auto;
+							display: block !important;
+						}
+	
+						#main-wrapper .page-content-wrapper #document-wrapper > div {
+							height: auto !important;
+							margin-top: 0 !important;
+							margin-bottom: 0 !important;
+						}
+	
+						#main-wrapper .page-content-wrapper #page-container-wrapper {
+							transform: scale(1) !important;
+							height: auto !important;
+							width: auto !important;
+						}
+	
+						#main-wrapper .page-content-wrapper #page-container > div {
+							margin: 0 !important;
+						}
+	
+						#main-wrapper {
+							width: 100vw !important;
+							height: auto !important;
+						}
+					}
+	
+					@media screen {
+						#main-wrapper .page-content-wrapper * {
+							display: none !important;
+						}
+					}
+				</style>
+			`;
+	
+			document.querySelectorAll("#main-wrapper > *").forEach(e => {
+				if (!e.classList.contains("page-content-wrapper")) {
+					e.remove();
+				}
+			});
+	
+			document.querySelectorAll("#main-wrapper .page-content-wrapper > *").forEach(e => {
+				if (e.id != "viewer-wrapper") {
+					e.remove();
+				}
+			});
+	
+			document.querySelectorAll("#main-wrapper .page-content-wrapper #viewer-wrapper > *").forEach(e => {
+				if (e.id != "document-wrapper") {
+					e.remove();
+				}
+			})
+	
+			document.querySelector("footer").remove();
+	
+			document.body.innerHTML += extraStyles;
+			
+			window.print();
+			document.body.innerHTML = originalContents;
+		}
 	}
 
 })();
